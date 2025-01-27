6475db214288933e8eaa82ad7d9d20a5f388eeee
Matheus Chiodi <matheuschiodi20@gmail.com>
2025-01-27 16:39:25 -0300

    feat(Accessories-container): add new container for Accessories products

---
+++ src/components/Accessories.jsx
@@ -0,0 +1,71 @@
+import { motion } from 'framer-motion';
+import { MoveRight, Star } from 'lucide-react';
+import { useRef } from 'react';
+import accessoriesProducts from '../data/accessoriesProducts';
+import CardProduct from './CardProduct';
+
+export default function Accessories() {
+  const carouselRef = useRef(null);
+
+  // Função para rolagem lateral do carrossel
+  const scrollLeft = () => {
+    carouselRef.current.scrollBy({ left: -200, behavior: 'smooth' });
+  };
+
+  const scrollRight = () => {
+    carouselRef.current.scrollBy({ left: 200, behavior: 'smooth' });
+  };
+
+  return (
+    <motion.div
+      className="mt-8 w-[98%] ms-[1%] flex flex-wrap lg:flex-nowrap justify-center items-center gap-6 px-6 py-6 bg-gradient-to-r from-white to-gray-100 rounded-3xl shadow-2xl"
+      initial={{ y: -50, opacity: 0 }}
+      animate={{
+        y: 0,
+        opacity: 1,
+        transition: { duration: 0.6, ease: 'easeOut' },
+      }}
+    >
+      <img
+        src={`./wallpapers/accessories.png`}
+        alt="Wheys"
+        className="w-[320px] h-[420px] object-cover rounded-2xl shadow-lg lg:block hidden"
+      />
+
+      <div className="w-full lg:w-[70%] flex flex-col">
+        <div className="flex items-center justify-start text-gray-900 gap-5">
+          <h2 className="text-[24px] md:text-[28px] lg:text-[36px] font-extrabold">
+            Discover Our Acessories
+          </h2>
+          <motion.div whileHover={{ scale: 1.05 }}>
+            <MoveRight className="text-[#00CFFD] cursor-pointer" size={32} />
+          </motion.div>
+        </div>
+
+        <div className="relative mt-4">
+          <button
+            className="absolute left-0 z-10 bg-white shadow-md p-3 rounded-full hidden md:block cursor-pointer hover:bg-gray-100"
+            onClick={scrollLeft}
+          >
+            ◀
+          </button>
+          <button
+            className="absolute right-0 z-10 bg-white shadow-md p-3 rounded-full hidden md:block cursor-pointer hover:bg-gray-100"
+            onClick={scrollRight}
+          >
+            ▶
+          </button>
+
+          <div
+            ref={carouselRef}
+            className="flex gap-4 overflow-x-auto scroll-smooth whitespace-nowrap px-1 no-scrollbar"
+          >
+            {accessoriesProducts.map((product) => (
+              <CardProduct product={product} key={product.id} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
---
+++ src/data/accessoriesProducts.js
@@ -0,0 +1,78 @@
+const accessoriesProducts = [
+  {
+    id: 1,
+    name: 'Shaker 700ml',
+    price: 'R$ 39.90',
+    benefits: ['BPA Free', 'Perfect Mix', 'Ergonomic Design'],
+    icon: '🥤',
+  },
+  {
+    id: 2,
+    name: 'Lifting Straps',
+    price: 'R$ 49.90',
+    benefits: ['Extra Support', 'Resistant Material', 'Prevents Injuries'],
+    icon: '🏋️',
+  },
+  {
+    id: 3,
+    name: 'Weightlifting Belt',
+    price: 'R$ 89.90',
+    benefits: ['Lumbar Protection', 'Adjustable', 'High Durability'],
+    icon: '🦾',
+  },
+  {
+    id: 4,
+    name: 'Workout Gloves',
+    price: 'R$ 59.90',
+    benefits: ['Greater Grip', 'Comfortable', 'Protects Hands'],
+    icon: '🧤',
+  },
+  {
+    id: 5,
+    name: '1L Thermal Bottle',
+    price: 'R$ 79.90',
+    benefits: ['Maintains Temperature', 'Resistant', 'Ideal for Long Workouts'],
+    icon: '🚰',
+  },
+  {
+    id: 6,
+    name: 'Myofascial Release Roller',
+    price: 'R$ 99.90',
+    benefits: ['Muscle Recovery', 'Relieves Pain', 'Ideal for Post-Workout'],
+    icon: '🌀',
+  },
+  {
+    id: 7,
+    name: 'Professional Jump Ropes',
+    price: 'R$ 69.90',
+    benefits: ['Cardio Intense', 'Light and Durable', 'Adjustable'],
+    icon: '🔗',
+  },
+  {
+    id: 8,
+    name: 'Push-Up Support',
+    price: 'R$ 89.90',
+    benefits: ['Improves Posture', 'Reduces Pressure on Hands', 'Adjustable'],
+    icon: '🏆',
+  },
+  {
+    id: 9,
+    name: 'Resistance Bands Kit',
+    price: 'R$ 129.90',
+    benefits: [
+      'Various Intensity Levels',
+      'Ideal for Mobility',
+      'Easy Transport',
+    ],
+    icon: '🛠️',
+  },
+  {
+    id: 10,
+    name: 'Digital Workout Stopwatch',
+    price: 'R$ 59.90',
+    benefits: ['High Precision', 'LED Screen', 'Long-Lasting Battery'],
+    icon: '⏱️',
+  },
+];
+
+export default accessoriesProducts;
