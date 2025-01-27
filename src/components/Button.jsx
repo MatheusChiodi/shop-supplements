171775eee74a937c57bfcbe3f6e7163ce1b7eed7
Matheus Chiodi <matheuschiodi20@gmail.com>
2025-01-27 15:00:29 -0300

    refactor(button): update design and improve animations

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
-import { Eye, Plus, X } from 'lucide-react';
+import { motion } from 'framer-motion';
 
-export default function Button({ title, onClick, icon }) {
+export default function Button({ title }) {
   return (
-    <button className="bg-gray-800 text-white p-2 px-4 rounded-lg hover:bg-[#ff5555] cursor-pointer flex items-center justify-center gap-3" onClick={onClick}>
+    <motion.button
+      className=" bg-[#00CFFD] text-white font-bold py-2 px-5 rounded-lg shadow-lg hover:bg-[#008fc7] transition-all cursor-pointer"
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+    >
       {title}
-
-      {icon === 'plus' && <Plus size={20} />}
-      {icon === 'eye' && <Eye size={20} />}
-      {icon === 'x' && <X size={20} />}
-    </button>
+    </motion.button>
   );
 }
